3309b1791add3f5754b79a15d19aad8f315b805b
x x <20201700449@cis.asu.edu.eg>
2024-04-17 08:41:17 +0200

    Product Picture Url Resolver

--- Talabat.APIs/Dtos/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Dtos/Helpers/MappingProfiles.cs
@@ -9,8 +9,8 @@ namespace Talabat.APIs.Dtos.Helpers
         {
 			CreateMap<Product, ProductToReturnDto>()
 				.ForMember(D => D.Brand, O => O.MapFrom(S => S.Brand.Name))
-				.ForMember(D => D.Category, O => O.MapFrom(S => S.Category.Name));
-
+				.ForMember(D => D.Category, O => O.MapFrom(S => S.Category.Name))
+				.ForMember(D => D.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
 		}
     }
 }
